f5034aeb1b6967103eedd6c6082e3644b9ff1fc1
Nicolò Rossi <nicolo.rossi@biters.it>
2025-11-27 10:36:41 +0100

    chore: added interceptor to add correlation id in log mdc context

---
+++ src/main/java/com/example/fabrick_interview_task1/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.fabrick_interview_task1.config;
+
+import com.example.fabrick_interview_task1.interceptor.CorrelationIdInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final CorrelationIdInterceptor correlationIdInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(correlationIdInterceptor)
+                .addPathPatterns("/**");
+    }
+}
